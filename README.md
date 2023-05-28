fe48a2e440a4e146970b24abf1d97c73d92e4124
waweroh <scadmsez@gmail.com>
2023-05-28 23:23:42 +0300

    twitter bot

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Twitter-bot
+This is a twitter bot repository that allows users to perform various actions on twitter using this bot through the twitter API.
+The project uses tweepy library to connect to the Twitter API using python.
+The bot has the following functions:
+<ol>
+1.  The tweet_bot function takes a message as input and optionally an image path. If an image path is provided, the code uploads the image and includes it in the tweet using the upload_media method. Finally, it creates a tweet using the create_tweet method and prints a success message with the tweet text
+2.    The retweet_bot function takes two parameters: search_query and num_retweets. It uses the Cursor object to search for tweets based on the given search query and retrieves the specified number of tweets. It then iterates over the retrieved tweets and attempts to retweet each tweet using the retweet method of the API object.
+3.  The like_tweets function takes two parameters: search_query (the query string to search for tweets) and num_likes (the number of tweets to like). It uses the search_recent_tweets method of the Client instance to search for recent tweets matching the query. It then iterates over the retrieved tweets and attempts to like each tweet using the like method 
+4.  The get_liked_tweets function takes in the username and number of likes parameters. It then iterates over the userid to retrieve the number of liked tweets requested for.
+</ol>
--- main.py
+++ main.py
@@ -1,19 +1,79 @@
 import tweepy
 import keys
 
-def api():
-    auth = tweepy.OAuthHandler(keys.api_key, keys.api_key_secret)
-    auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-    return tweepy.API(auth)
 
-def tweet(api: tweepy.API, message: str, image_path: str=None):
+# Intitalize the twitter client
+client = tweepy.Client(
+    consumer_key=keys.api_key,
+    consumer_secret=keys.api_key_secret,
+    bearer_token = keys.bear_token,
+    access_token=keys.access_token,
+    access_token_secret=keys.access_token_secret
+)
+auth = tweepy.OAuth1UserHandler(
+    access_token=keys.access_token,
+    access_token_secret=keys.access_token_secret,
+    consumer_key=keys.api_key,
+    consumer_secret=keys.api_key_secret
+)
+api = tweepy.API(auth)
+
+# Send a tweet
+client.create_tweet(text="Have you a great day. Happy coding.")
+def like_tweets(search_query, num_likes):
+    tweets = client.search_all_tweets(query=search_query, tweet_fields="id", max_results=num_likes)
+    for tweet in tweets:
+        try:
+            client.like(tweet.id)
+            print("Liked a tweet:", tweet.text)
+        except tweepy.TweepError as e:
+            print("Error liking tweet:", e)
+
+# usage: like tweets containing the hashtag "#haters"
+
+
+
+def retweet_bot(search_query, num_retweets):
+    tweets = client.search_recent_tweets(query=search_query, tweet_fields="id", max_results=num_retweets)
+    for tweet in tweets.data:
+        try:
+            # Retweet the tweet
+            client.retweet(tweet.id)
+            print("Retweeted a tweet:", tweet.text)
+        except tweepy.TweepError as e:
+            print("Error retweeting tweet:", e)
+
+# usage: retweet tweets containing hashtag "#Elonmusk"
+
+
+def tweet_bot(message, image_path=None):
     if image_path:
-        api.update_status_with_media(message, image_path)
+        media = client.upload_media(image_path)
+        response = client.create_tweet(text=message, media_ids=[media.media_key])
     else:
-        api.update_status(message)
-    print ("Tweeted successfully")
+        response = client.create_tweet(text=message)
+    tweet = response.data
+    print("Tweeted successfully:", tweet.text)
+
+# usage: tweet a message 
+
+def get_liked_tweets(username, num_tweets):
+    liked_tweets = client.get_user_liked_tweets(username=username, max_results=num_tweets)
+    for tweet in liked_tweets.data:
+        try:
+            print("Tweet ID:", tweet.id)
+            print("Text:", tweet.text)
+            print("Likes:", tweet.public_metrics.like_count)
+            print("Retweets:", tweet.public_metrics.retweet_count)
+            print("---")
+        except tweepy.errors.Forbidden as e:
+            print("Forbidden error:", e)
 
 if __name__ == "__main__":
-    api = api()
-    tweet(api, 'hehe. Did this stop happening')
+    tweet_bot("assemble coders.")
+    retweet_bot("#Elonmusk", 5)
+    get_liked_tweets("wdkjkhj", 15)
+    like_tweets("#haters", 20) 
+
+
